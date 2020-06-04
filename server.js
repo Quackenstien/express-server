69e1d9e1586191391522d370e2c243a82af14aa3
Quackenstien <donaldhawkins422@gmail.com>
2020-06-03 18:32:16 -0700

    moved level

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var fs = require("fs");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -17,7 +17,7 @@ app.use(express.json());
 // =============================================================
 
 //HTML Routes
-app.get("*", function (req, res) {
+app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
